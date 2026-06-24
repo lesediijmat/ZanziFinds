1ebe4cead1d7000e20b7e3f9d83c6545f91acf9b
lesediijmat <lesediij.mat@gmail.com>
2026-06-24 17:32:02 +0200

    Update update_qty.php

--- update_qty.php
+++ update_qty.php
@@ -1,25 +1,24 @@
 <?php
-include 'config.php';
+session_start();
+header('Content-Type: application/json');
 
-if(!isset($_SESSION['cart'])){
-    $_SESSION['cart'] = [];
-}
-
-$id = $_POST['id'];
+$id = $_POST['id'] ?? null;
 $change = intval($_POST['change']);
 
-if(isset($_SESSION['cart'][$id])){
+if (!$id || !isset($_SESSION['cart'][$id])) {
+    echo json_encode(["status" => "error"]);
+    exit;
+}
 
-    $_SESSION['cart'][$id]['qty'] += $change;
+$_SESSION['cart'][$id]['qty'] += $change;
 
-    if($_SESSION['cart'][$id]['qty'] < 1){
-        $_SESSION['cart'][$id]['qty'] = 1;
-    }
+if ($_SESSION['cart'][$id]['qty'] < 1) {
+    $_SESSION['cart'][$id]['qty'] = 1;
 }
 
 $subtotal = 0;
 
-foreach($_SESSION['cart'] as $item){
+foreach ($_SESSION['cart'] as $item) {
     $subtotal += $item['price'] * $item['qty'];
 }
 
@@ -27,6 +26,7 @@ $delivery = ($subtotal > 0) ? 20 : 0;
 
 echo json_encode([
     "status" => "success",
+    "id" => $id,
     "qty" => $_SESSION['cart'][$id]['qty'],
     "subtotal" => $subtotal,
     "total" => $subtotal + $delivery,
